add0d8e0702b493e0c6e81942f0b90daad4e4d60
CalebSandman <157178488+CalebSandman@users.noreply.github.com>
2025-05-02 12:43:01 -0400

    Update Fab_Code_Ultrasonic_Turning.ino
    - More comments
    - Deleted old code that was previously commented out

--- Fab_Code_Ultrasonic_Turning.ino
+++ Fab_Code_Ultrasonic_Turning.ino
@@ -3,17 +3,17 @@ Haunted Mechanics LLC
 Written by Sandman
 */
 
-int trigPinL = 7;  //connects the "trig" pin from the ultrasonic sensor to a digital pin (7 for testing)
-int echoPinL = 8;  //connects the "echo" pin from the ultrasonic sensor to a digital pin (8 for testing)
+int trigPinL = 7;  //connects the "trig" pin from the left ultrasonic sensor to digital pin 7
+int echoPinL = 8;  //connects the "echo" pin from the left ultrasonic sensor to digital pin 8
 
-int trigPinR = 1;
-int echoPinR = 2;
+int trigPinR = 1; //connects the "trig" pin from the right ultrasonic sensor to digital pin 1
+int echoPinR = 2; //connects the "echo" pin from the right ultrasonic sensor to digital pin 2
 
 float durationL, distanceL, durationR, distanceR, distanceAvg, distanceDif; //initializes duration and distance var
 
 char move = 'I';
 
-//variable names for the motors
+//variable names for the motors. Connected to digital pins of the same number
 int in1 = 4;
 int in2 = 5;
 int in3 = 9;
@@ -43,14 +43,17 @@ void setup()
 void loop()
 {
   //starts the trig pin at low, waits, changes it to high, waits, changes it back to low
+  //left sensor, 2 microsecond delay > 5 micro second delay
   digitalWrite(trigPinL, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPinL, HIGH);
   delayMicroseconds(5);
   digitalWrite(trigPinL, LOW);
 
+  //10 microsecond delay before the right sensor fires to avoid interference
   delayMicroseconds(10);
 
+  //right sensor, 2 microsecond delay > 5 micro second delay
   digitalWrite(trigPinR, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPinR, HIGH);
@@ -58,12 +61,15 @@ void loop()
   digitalWrite(trigPinR, LOW);
 
   //reads the duration the echo and converts the time into a distance in cm
+  //left sensor distance calculation
   durationL = pulseIn(echoPinL, HIGH);
   distanceL = durationL * 0.034/2;
 
+  //right sensor distance calculation
   durationR = pulseIn(echoPinR, HIGH);
   distanceR = durationR * 0.034/2;
 
+  //average and difference between sensor distances
   distanceAvg = (distanceL+distanceR)/2; //average distance between both sensors
   distanceDif = abs(distanceL-distanceR); //the difference in distance between both sensors
 
@@ -76,6 +82,7 @@ void loop()
   Serial.print(distanceR);
   Serial.println(" cm");
 
+  //process of determining if robot should move, then if forward or backward, then ir turn left or right
   if((distanceL > 100 || distanceL == 0) && (distanceR > 100 || distanceR == 0)) //if both sensors report a distance of 0cm or greater than 100cm - Idle
     move = 'I';
   else if(distanceDif <= 10) //else, if the sensors are reporting distances within 10cm of each other
@@ -95,6 +102,7 @@ void loop()
       move = 'R';
   }
 
+//switch statment containing the code for all forms of movement (forward, backward, turn left, turn right, idle)
 switch(move)
 {
   case 'F': //move forward
@@ -136,41 +144,6 @@ switch(move)
     digitalWrite(in4, LOW);
     Serial.println("I");
 }
-  /*
-  //movement backward when object within 10cm
-  if(distanceL <= 10 && distanceR <= 10)
-  {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-  }
-  //idle when object between between 10cm and 15cm
-  else if(distanceL <= 15 && distanceR <= 15)
-  {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-  }
-  //movement foward when object within 15cm and 45cm
-  else if(distanceL <= 45 && distanceR <= 45)
-  {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  }
-  //idle when object greater than 45cm away
-  else
-  {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-  }
- */
-
   /*
   short delay before looping. Breaks without it. 
   Why? No idea, but I do know that it works.
